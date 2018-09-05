8319f28dedf76100abc08dd7ea7f4eee4cdb4864
locdeqe <245586@niuitmo.ru>
2018-09-05 14:42:40 +0300

    Titanic Decision Tree

--- TitanicDecisionTree/main.py
+++ TitanicDecisionTree/main.py
@@ -1,17 +1,28 @@
 import pandas
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+def visualize_tree(tree, feature_names):
+    with open("dt.dot", 'w') as f:
+        export_graphviz(tree, out_file=f,
+                        feature_names=feature_names)
+
+def preprocess_data(data):
+    selectedData = data[['Pclass', 'Fare', 'Sex', 'Age']]
+    selectedData = selectedData[selectedData['Age'] > 0]
+    selectedData['Sex'] = selectedData['Sex'].map({'female': 0, 'male': 1})
+
+    return selectedData
 
 data = pandas.read_csv('./data/titanic.csv', index_col="PassengerId")
-selectedData = data[['Pclass', 'Fare', 'Sex', 'Age']]
-selectedData = selectedData[selectedData['Age'] > 0]
 
-selectedData['Sex'] = selectedData['Sex'].map({'female': 1, 'male': 0})
+preprocessed_data = preprocess_data(data)
+feature_names = list(preprocessed_data.columns[:])
 
 targetData = data[['Survived']]
 targetData = targetData[data['Age'] > 0]
 
 clf = DecisionTreeClassifier(random_state=241)
 
-clf.fit(selectedData, targetData)
+clf.fit(preprocessed_data, targetData)
 
-print(clf.feature_importances_)
+visualize_tree(clf, feature_names)
