a04799112163a1b1a8e15c928ce9a66fcd6c19ea
v.karasev <245586@niuitmo.ru>
2018-11-20 19:06:09 +0300

    tf-idf

--- TF-IDF SVM/main.py
+++ TF-IDF SVM/main.py
@@ -12,11 +12,11 @@ def writeAnswer(path, answer):
     file.close()
 
 newGroups = datasets.fetch_20newsgroups(subset = 'all', categories = ['alt.atheism', 'sci.space'])
-vectorizer  = TfidfVectorizer(stop_words = 'english')
+vectorizer  = TfidfVectorizer()
 
 X = vectorizer.fit_transform(newGroups.data)
 y = newGroups.target
-'''
+
 grid = {'C': np.power(10.0, np.arange(-5, 5))}
 cv = KFold(n_splits = 5, shuffle = True, random_state = 241)
 model = SVC(kernel = 'linear', random_state = 241)
@@ -25,26 +25,22 @@ gs = GridSearchCV(model, grid, scoring = 'accuracy', cv = cv)
 gs.fit(X, y)
 
 coef = gs.best_params_
-'''
-newModel = SVC(C = 10, kernel = 'linear', random_state = 241)
-newModel.fit(X, y)
 
-afterFitCoefs = newModel.coef_.toarray()
+newModel = SVC(C = coef.get('C'), kernel = 'linear', random_state = 241)
 
-for x in afterFitCoefs:
-    x = abs(x)
+newModel.fit(vectorizer.transform(newGroups.data), y)
 
+afterFitCoefs = newModel.coef_
+row = afterFitCoefs.getrow(0).toarray()[0].ravel()
+idx = np.argsort(abs(row))[-10:]
 
-'''
-N = 10
-
-idx = np.ravel(X.sum(axis=0).argsort(axis=1))[::-1][:N]
 top10Words = np.array(vectorizer.get_feature_names())[idx].tolist()
 
-answer = ' '.join(top10Words)
+top10Words.sort()
+
+answer = ','.join(top10Words)
 
 if not os.path.exists('./result'):
     os.makedirs('./result')
 
 writeAnswer('./result/answer.txt', answer)
-'''
